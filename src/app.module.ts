aaa7038fc0743477b5b4bf786dde70052906c398
Dung Do <dung_do@datahouse.com>
2024-11-06 20:36:53 +0700

    init: eslint, prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { FilmModule } from './film/film.module';
+import { PrismaService } from './prisma-module/prisma.service';
+import { PrismaModule } from './prisma-module/prisma-module.module';
 
 @Module({
-  imports: [FilmModule],
+  imports: [PrismaModule, FilmModule],
   controllers: [AppController],
-  providers: [AppService  ]
+  providers: [AppService, PrismaService],
 })
-export class AppModule {
-}
+export class AppModule {}
